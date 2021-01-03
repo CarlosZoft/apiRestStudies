15ae5d37d0f0ea18f4bfdec3d40c8b605d55525e
Carlos <carlosoftwar@gmail.com>
2021-01-03 15:37:34 -0300

    separating http verbs

--- server_apirest/server.js
+++ server_apirest/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const data = require('./data');
 
 
 // Verbos HTTP 
@@ -9,12 +10,48 @@ const app = express();
 // PUT  Atualiazar dados de um Resource
 // DELETE   Deletar dados de um Resource
 
-app.get("/clients",);
-app.get("/clients/:id",);
-app.post("/clients",);
-app.put("/clients/:id",);
-app.delete("/clients/:id",);
+app.use(express.json());
 
+
+app.get("/clients",(req,res)=>{
+    res.json(data);
+});
+
+app.get("/clients/:id",(req,res)=>{
+    const id = req.params.id;
+    const client = data.find(cliente => cliente.id == id);
+    if(client)res.json(client);
+    else res.status(204).json();
+        
+});
+
+app.post("/clients",(req,res)=>{
+    const {name , email} = req.body;
+    
+    //salvar
+  
+    req.json({name, email});
+});
+
+app.put("/clients/:id",(req,res)=>{
+    const id = req.params.id;
+    const client = data.find(cliente => cliente.id == id);
+    
+    if(client){
+        const {name} = req.body;
+        client.name = name;
+        res.json(client);
+    }
+    else res.status(204).json();
+
+});
+app.delete("/clients/:id",(req,res)=>{
+    const id = req.params.id;
+    const clientFiltered = data.filter(cliente => cliente.id != id);
+
+    res.json(clientFiltered);
+
+});
 
 app.listen(3333, () => {
     console.log("Server On");
